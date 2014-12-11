431bf889266b784aedbab48ad9027308a746f5a5
peterhad313 <peterhaddad2016@u.northwestern.edu>
2014-12-10 20:21:14 -0600

    redo l1cache

--- source/l1Cache.vhd
+++ source/l1Cache.vhd
@@ -7,76 +7,55 @@ entity l1Cache is
 	addr  : in std_logic_vector (31 downto 0);
 	write_data : in std_logic_vector(31 downto 0);
 	writeEn : in std_logic;
+	Memory_Valid_Write: in std_logic;
+ 	Memory_Block_Data_Valid : in std_logic;
+   	L2_hit: in std_logic;
+   	Enable: in std_logic;
+   	L2_Block_In: std_logic_vector(511 downto 0);
+
 
 	hit : out std_logic;
 	miss : out std_logic;
 	dataOut: out std_logic_vector(31 downto 0);
-	writeBack: out std_logic --Eviction notice, written back to cache
+	data_out_to_L2: out std_logic_vector(511 downto 0);
+	Write_Main_Memory: out std_logic --Eviction notice, written back to cache
 
   ) ;
 end entity ; -- l1Cache
 
 architecture struct of l1Cache is
 
-	signal tag : std_logic_vector(23 downto 0);
-	signal blockOffset : std_logic_vector(3 downto 0);
-	signal setIndex : std_logic_vector(3 downto 0);
+	signal tagFromAddr, L1_current_tag: std_logic_vector (21 downto 0);
+    signal indexFromAddr: std_logic_vector ( 3 downto 0);
+    signal offsetFromAddr, offset_inv: std_logic_vector ( 5 downto 0);
+    
+    signal L1WriteEnable, WrEn_L1_pc_d, WrEn_L1_pc, dirty_bit_comp, tag_match, tag_miss_dff, tag_miss, h0, h1, validFromL1, tag_match_dff: std_logic;
+    signal L1_Block_Out, L1_Block_In, L1_hit_block_In, L1_Hit_Data_In, L2_Block_In_T  : std_logic_vector ( 534 downto 0);
+    signal m0, m1, m2, m3, s1, s0, L1_hit_block_In_wdt, L1_Block_In_wdt, L1_Block_shifted : std_logic_vector ( 511 downto 0);
+    
+    
+begin 
 
-	signal readEn : std_logic;
-	signal set_0_Out, set_1_Out, set_2_Out, set_3_Out, set_4_Out, set_5_Out, set_6_Out, set_7_Out, set_8_Out, set_9_Out, set_10_Out, set_11_Out, set_12_Out, set_13_Out, set_14_Out, set_15_Out : std_logic_vector(31 downto 0);
-	signal valid_0_Out, valid_1_Out, valid_2_Out, valid_3_Out, valid_4_Out, valid_5_Out, valid_6_Out, valid_7_Out, valid_8_Out, valid_9_Out, valid_10_Out, valid_11_Out, valid_12_Out, valid_13_Out, valid_14_Out, valid_15_Out : std_logic;
-	signal valid_0_in, valid_1_in, valid_2_in, valid_3_in, valid_4_in, valid_5_in, valid_6_in, valid_7_in, valid_8_in, valid_9_in, valid_10_in, valid_11_in, valid_12_in, valid_13_in, valid_14_in, valid_15_in : std_logic;
+tagFromAddr <= addr ( 31 downto 10);
+indexFromAddr <= addr (9 downto 6);
+offsetFromAddr <= addr (5 downto 0);
 
-	signal tag_0_Out, tag_1_Out, tag_2_Out, tag_3_Out, tag_4_Out, tag_5_Out, tag_6_Out, tag_7_Out, tag_8_Out, tag_9_Out, tag_10_Out, tag_11_Out, tag_12_Out, tag_13_Out, tag_14_Out, tag_15_Out : std_logic_vector(23 downto 0);
+--535 by 16 memory
+L1_csram: entity work.csram generic map ( INDEX_WIDTH => 4, BIT_WIDTH => 535 )
+                port map ( cs => '1', oe => '1', we => L1WriteEnable, index => indexFromAddr, din => L1_Block_In, dout => L1_Block_Out);
 
-	--signal tag_0_in, tag_1_in, tag_2_in, tag_3_in, tag_4_in, tag_5_in, tag_6_in, tag_7_in, tag_8_in, tag_9_in, tag_10_in, tag_11_in, tag_12_in, tag_13_in, tag_14_in, tag_15_in : std_logic_vector(23 downto 0);
+L1_current_tag <= L1_Block_Out(533 downto 512);
+validFromL1 <= L1_Block_Out(534);
 
-	signal writeSetDecoded : std_logic_vector(15 downto 0);
-
-	signal tagsMuxed : std_logic_vector(23 downto 0);
-	signal tagCmp: std_logic;
-	signal validsMuxed : std_logic;
-	signal dataMuxed : std_logic_vector(31 downto 0);
-	signal hit_temp : std_logic;
-
-begin
-tag<=addr(31 downto 8);
-setIndex<= addr(7 downto 4);
-blockOffset<= addr(3 downto 0);
-readEn<= not writeEn;
+Comparator_L1: cmp_n generic map ( n => 22 ) port map ( a => L1_current_tag, b => tagFromAddr, a_eq_b => tag_match); 
 
 
-dec0: entity work.dec_4 port map (setIndex, writeSetDecoded);
-set0: entity work.SetL1 port map (clock,valid_0_in, tag,write_data,writeSetDecoded(0),	blockOffset, valid_0_out, tag_0_out, set_0_Out);
-set1: entity work.SetL1 port map (clock,valid_1_in, tag,write_data,writeSetDecoded(1),	blockOffset, valid_1_out, tag_1_out, set_1_Out);
-set2: entity work.SetL1 port map (clock,valid_2_in, tag,write_data,writeSetDecoded(2),	blockOffset, valid_2_out, tag_2_out, set_2_Out);
-set3: entity work.SetL1 port map (clock,valid_3_in, tag,write_data,writeSetDecoded(3),	blockOffset, valid_3_out, tag_3_out, set_3_Out);
-set4: entity work.SetL1 port map (clock,valid_4_in, tag,write_data,writeSetDecoded(4),	blockOffset, valid_4_out, tag_4_out, set_4_Out);
-set5: entity work.SetL1 port map (clock,valid_5_in, tag,write_data,writeSetDecoded(5),	blockOffset, valid_5_out, tag_5_out, set_5_Out);
-set6: entity work.SetL1 port map (clock,valid_6_in, tag,write_data,writeSetDecoded(6),	blockOffset, valid_6_out, tag_6_out, set_6_Out);
-set7: entity work.SetL1 port map (clock,valid_7_in, tag,write_data,writeSetDecoded(7),	blockOffset, valid_7_out, tag_7_out, set_7_Out);
-set8: entity work.SetL1 port map (clock,valid_8_in, tag,write_data,writeSetDecoded(8),	blockOffset, valid_8_out, tag_8_out, set_8_Out);
-set9: entity work.SetL1 port map (clock,valid_9_in, tag,write_data,writeSetDecoded(9),	blockOffset, valid_9_out, tag_9_out, set_9_Out);
-set10: entity work.SetL1 port map (clock,valid_10_in, tag,write_data,writeSetDecoded(10),	blockOffset, valid_10_out, tag_10_out, set_10_Out);
-set11: entity work.SetL1 port map (clock,valid_11_in, tag,write_data,writeSetDecoded(11),	blockOffset, valid_11_out, tag_11_out, set_11_Out);
-set12: entity work.SetL1 port map (clock,valid_12_in, tag,write_data,writeSetDecoded(12),	blockOffset, valid_12_out, tag_12_out, set_12_Out);
-set13: entity work.SetL1 port map (clock,valid_13_in, tag,write_data,writeSetDecoded(13),	blockOffset, valid_13_out, tag_13_out, set_13_Out);
-set14: entity work.SetL1 port map (clock,valid_14_in, tag,write_data,writeSetDecoded(14),	blockOffset, valid_14_out, tag_14_out, set_14_Out);
-set15: entity work.SetL1 port map (clock,valid_15_in, tag,write_data,writeSetDecoded(15),	blockOffset, valid_15_out, tag_15_out, set_15_Out);
+--set the hit and miss
+hit <= tag_match;
+L1_Miss <= tag_miss;
+miss_sig_map: not_gate port map (tag_match, tag_miss);
 
-mux1: entity work.mux_16_to_1 port map (setIndex, tag_0_Out, tag_1_Out, tag_2_Out, tag_3_Out, tag_4_Out,tag_5_Out,tag_6_Out, tag_7_Out, 
-								 		tag_8_Out,tag_9_Out, tag_10_Out, tag_11_Out, tag_12_Out,tag_13_Out, tag_14_Out, tag_15_Out, tagsMuxed);
 
-mux2: entity work.mux_16_to_1_bit port map (setIndex, valid_0_Out, valid_1_Out, valid_2_Out, valid_3_Out, valid_4_Out,valid_5_Out,valid_6_Out, valid_7_Out, 
-								 		valid_8_Out,valid_9_Out, valid_10_Out, valid_11_Out, valid_12_Out,valid_13_Out, valid_14_Out, valid_15_Out, validsMuxed);
 
-mux3: entity work.mux_16_to_1_32bit port map (setIndex, set_0_Out, set_1_Out, set_2_Out, set_3_Out, set_4_Out,set_5_Out,set_6_Out, set_7_Out, 
-								 		set_8_Out,set_9_Out, set_10_Out, set_11_Out, set_12_Out,set_13_Out, set_14_Out, set_15_Out, dataMuxed);
-
-cmp: entity work.cmp_n generic map (n=>24) port map (a=>tagsMuxed, b=>tag, a_eq_b=>tagCmp);
-and0: entity work.and_gate port map (tagCmp, validsMuxed, hit_temp);
-not0: entity work.not_gate port map (hit_temp, miss);
-hit<=hit_temp;
-dataOut<= dataMuxed;
 
 end struct; -- struct
